19589cc27241a1ad44b688a841a02d1b88c8fdb5
Andrew Watson <andrewwatson@Andrews-MacBook-Pro.local>
2022-07-15 14:57:16 +0100

    add some clarifying comments and rename some vars/methods

--- src/main/kotlin/com/deliveroo/cron/CronParser.kt
+++ src/main/kotlin/com/deliveroo/cron/CronParser.kt
@@ -4,30 +4,33 @@ import com.deliveroo.errorhandling.InvalidCronException
 
 object CronParser {
 
-    private val minutesRangeRegex = Regex("^(\\d|[0-5]\\d)-(\\d|[0-5]\\d)\$")
+    //regex for a minutes field for a range of values, e.g. 5-10
+    private val minutesRangeRegex = Regex("^(?:\\d|[0-5]\\d)-(?:\\d|[0-5]\\d)\$")
+
+    //regex for a minutes field that contains a step, ie a slash, e.g. */15
     private val minutesStepRegex = Regex("^(\\*|(?:\\d|[0-5]\\d)-(?:\\d|[0-5]\\d))/(\\d|[1-2]\\d|30)\$")
-    private val defaultMinutesRegex = Regex("^(\\d|[1-5][\\d)])(,\\d|[1-5][\\d)])*\$")
+
+    //regex for a minutes field that contains a single value or a comma separated list, e.g. 5 or 5,6,7,8
+    private val listOfSpecificMinutesRegex = Regex("^(?:\\d|[1-5][\\d)])(?:,(?:\\d|[1-5][\\d)]))*\$")
 
     fun parse(string: String) {
         throw InvalidCronException("Invalid cron statement, statement must have 6 fields (minute, hour, day of month, month, day of week, command)")
     }
 
     fun parseMinutes(minutes: String): String {
-        if (minutes == "*") return minutesStepString()
+        if (minutes == "*") return minutesValuesWithStep()
         if (minutesStepRegex.matches(minutes)) {
             val matchResult = minutesStepRegex.find(minutes)!!
-            val stepRange = matchResult.groupValues[1]
             val stepMinutes = Integer.parseInt(matchResult.groupValues[2])
-            val (start, end) = findRange(stepRange)
-            return minutesStepString(start, end, stepMinutes)
+            val (start, end) = findRange(matchResult.groupValues[1])
+            return minutesValuesWithStep(start, end, stepMinutes)
         }
         if (minutesRangeRegex.matches(minutes)) {
             val matchResult = minutesRangeRegex.find(minutes)!!
-            val start = Integer.parseInt(matchResult.groupValues[1])
-            val end = Integer.parseInt(matchResult.groupValues[2])
-            return minutesStepString(start, end)
+            val (start, end) = findRange(matchResult.groupValues[0])
+            return minutesValuesWithStep(start, end)
         }
-        if (defaultMinutesRegex.matches(minutes)) {
+        if (listOfSpecificMinutesRegex.matches(minutes)) {
             return "minute ${minutes.split(",").joinToString(separator = " ")}"
         }
         throw InvalidCronException("Invalid input for minutes field")
@@ -39,7 +42,6 @@ object CronParser {
         return Pair(Integer.parseInt(range[0]), Integer.parseInt(range[1]))
     }
 
-    private fun minutesStepString(start: Int = 0, end: Int = 59, stepMinutes: Int = 1) : String{
-        return "minute ${(start..end step stepMinutes).joinToString(" ")}"
-    }
+    private fun minutesValuesWithStep(start: Int = 0, end: Int = 59, step: Int = 1) =
+        "minute ${(start..end step step).joinToString(" ")}"
 }

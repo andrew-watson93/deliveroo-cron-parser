28447e8c0b398167b8f93d0353f9bc974288289f
Andrew Watson <andrewjw64@gmail.com>
2022-07-16 12:14:12 +0100

    Add asterisk step functionality to hour field

--- src/main/kotlin/com/deliveroo/cron/HourFieldParser.kt
+++ src/main/kotlin/com/deliveroo/cron/HourFieldParser.kt
@@ -5,14 +5,20 @@ private const val SPACE = " "
 object HourFieldParser {
 
     private val rangeRegex = Regex("^(\\d|1\\d|2[0-3])-(\\d|1\\d|2[0-3])\$")
+    private val stepRegex = Regex("^(\\*|(\\d|1\\d|2[0-3])-(\\d|1\\d|2[0-3]))/(\\d|1[0-2])\$")
 
     fun parse(hourField: String): String {
         if (hourField == "*") return "hour ${(0..23).joinToString(separator = SPACE)}"
         if (rangeRegex.matches(hourField)) {
-            val matchResult = rangeRegex.find(hourField)
-            val start = Integer.parseInt(matchResult!!.groupValues[1])
+            val matchResult = rangeRegex.find(hourField)!!
+            val start = Integer.parseInt(matchResult.groupValues[1])
             val end = Integer.parseInt(matchResult.groupValues[2])
             return "hour ${(start..end).joinToString(separator = SPACE)}"
+        }
+        if (stepRegex.matches(hourField)) {
+            val matchResult = stepRegex.find(hourField)!!
+            val step = Integer.parseInt(matchResult.groupValues[4])
+            return "hour ${(0..23 step step).joinToString(separator = SPACE)}"
         }
         return "hour ${hourField.split(",").joinToString(separator = SPACE)}"
     }
--- src/test/kotlin/com/deliveroo/cron/HourFieldParserTest.kt
+++ src/test/kotlin/com/deliveroo/cron/HourFieldParserTest.kt
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test
 internal class HourFieldParserTest {
 
     @Test
-    fun `Single hour fields are supported`(){
+    fun `Single hour fields are supported`() {
         assertThat(HourFieldParser.parse("0")).isEqualTo("hour 0")
     }
 
     @Test
-    fun `Multiple hour fields separated by commas are supported`(){
+    fun `Multiple hour fields separated by commas are supported`() {
         assertThat(HourFieldParser.parse("0,1,2,3")).isEqualTo("hour 0 1 2 3")
     }
 
@@ -22,7 +22,13 @@ internal class HourFieldParserTest {
     }
 
     @Test
-    fun `Hours field should support a single asterisk`(){
+    fun `Hours field should support a single asterisk`() {
         assertThat(HourFieldParser.parse("*")).isEqualTo("hour 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23")
+    }
+
+    @Test
+    fun `Hours field should support asterisk with a step`() {
+        val hours = "*/4"
+        assertThat(HourFieldParser.parse(hours)).isEqualTo("hour 0 4 8 12 16 20")
     }
 }

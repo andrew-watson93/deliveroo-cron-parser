c7065217909d0d563299b33a7bd3cdb974a3b774
Andrew Watson <andrewwatson@Andrews-MacBook-Pro.local>
2022-07-16 10:18:48 +0100

    Simplify logic in minutes field using capture groups more effectively

--- src/main/kotlin/com/deliveroo/cron/MinutesFieldParser.kt
+++ src/main/kotlin/com/deliveroo/cron/MinutesFieldParser.kt
@@ -6,10 +6,10 @@ import com.deliveroo.errorhandling.InvalidCronException
 object MinutesFieldParser {
 
     //regex for a minutes field for a range of values, e.g. 5-10
-    private val minutesRangeRegex = Regex("^(?:\\d|[0-5]\\d)-(?:\\d|[0-5]\\d)\$")
+    private val minutesRangeRegex = Regex("^(\\d|[0-5]\\d)-(\\d|[0-5]\\d)\$")
 
     //regex for a minutes field that contains a step, ie a slash, e.g. */15
-    private val minutesStepRegex = Regex("^(\\*|(?:\\d|[0-5]\\d)-(?:\\d|[0-5]\\d))/(\\d|[1-2]\\d|30)\$")
+    private val minutesStepRegex = Regex("^(\\*|(\\d|[0-5]\\d)-(\\d|[0-5]\\d))/(\\d|[1-2]\\d|30)\$")
 
     //regex for a minutes field that contains a single value or a comma separated list, e.g. 5 or 5,6,7,8
     private val listOfSpecificMinutesRegex = Regex("^(?:\\d|[1-5][\\d)])(?:,(?:\\d|[1-5][\\d)]))*\$")
@@ -19,25 +19,24 @@ object MinutesFieldParser {
         if (minutesField == "*") return minutesValuesWithStep()
         if (minutesStepRegex.matches(minutesField)) {
             val matchResult = minutesStepRegex.find(minutesField)!!
-            val stepMinutes = Integer.parseInt(matchResult.groupValues[2])
-            val (start, end) = findRange(matchResult.groupValues[1])
+            val (start, end) = if (matchResult.groupValues[1] == "*") {
+                Pair(0, 59)
+            } else {
+                Pair(Integer.parseInt(matchResult.groupValues[2]), Integer.parseInt(matchResult.groupValues[3]))
+            }
+            val stepMinutes = Integer.parseInt(matchResult.groupValues[4])
             return minutesValuesWithStep(start, end, stepMinutes)
         }
         if (minutesRangeRegex.matches(minutesField)) {
             val matchResult = minutesRangeRegex.find(minutesField)!!
-            val (start, end) = findRange(matchResult.groupValues[0])
+            val start = Integer.parseInt(matchResult.groupValues[1])
+            val end = Integer.parseInt(matchResult.groupValues[2])
             return minutesValuesWithStep(start, end)
         }
         if (listOfSpecificMinutesRegex.matches(minutesField)) {
             return "minute ${minutesField.split(",").joinToString(separator = " ")}"
         }
         throw InvalidCronException("Invalid input for minutes field")
-    }
-
-    private fun findRange(stepRange: String): Pair<Int, Int> {
-        if (stepRange == "*") return Pair(0, 59)
-        val range = stepRange.split("-")
-        return Pair(Integer.parseInt(range[0]), Integer.parseInt(range[1]))
     }
 
     private fun minutesValuesWithStep(start: Int = 0, end: Int = 59, step: Int = 1) =

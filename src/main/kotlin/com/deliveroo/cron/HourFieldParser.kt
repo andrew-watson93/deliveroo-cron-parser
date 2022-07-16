dfd738c928b7f7d1522924c3653abc74cb1b0cbb
Andrew Watson <andrewwatson@Andrews-MacBook-Pro.local>
2022-07-16 09:47:32 +0100

    Add range functionality for hours field, fix minutes field parser tests

--- src/main/kotlin/com/deliveroo/cron/HourFieldParser.kt
+++ src/main/kotlin/com/deliveroo/cron/HourFieldParser.kt
@@ -1,7 +1,16 @@
 package com.deliveroo.cron
 
 object HourFieldParser {
+
+    private val rangeRegex = Regex("^(\\d|1\\d|2[0-3])-(\\d|1\\d|2[0-3])\$")
+
     fun parse(hourField: String): String {
+        if (rangeRegex.matches(hourField)) {
+            val matchResult = rangeRegex.find(hourField)
+            val start = Integer.parseInt(matchResult!!.groupValues[1])
+            val end = Integer.parseInt(matchResult.groupValues[2])
+            return "hour ${(start..end).joinToString(separator = " ")}"
+        }
         return "hour ${hourField.split(",").joinToString(separator = " ")}"
     }
 
--- src/test/kotlin/com/deliveroo/cron/HourFieldParserTest.kt
+++ src/test/kotlin/com/deliveroo/cron/HourFieldParserTest.kt
@@ -1,7 +1,6 @@
 package com.deliveroo.cron
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class HourFieldParserTest {
@@ -14,5 +13,11 @@ internal class HourFieldParserTest {
     @Test
     fun `Multiple hour fields separated by commas are supported`(){
         assertThat(HourFieldParser.parse("0,1,2,3")).isEqualTo("hour 0 1 2 3")
+    }
+
+    @Test
+    fun `Minutes field should support a range of digits greater than 0`() {
+        val hours = "0-5"
+        assertThat(HourFieldParser.parse(hours)).isEqualTo("hour 0 1 2 3 4 5")
     }
 }

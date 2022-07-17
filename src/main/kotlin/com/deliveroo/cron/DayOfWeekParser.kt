732c6dfd36d0d61fe3d5b33266a9b0a37863cad4
Andrew Watson <andrewjw64@gmail.com>
2022-07-17 08:13:51 +0100

    Add multiple day of week functionality

--- src/main/kotlin/com/deliveroo/cron/DayOfWeekParser.kt
+++ src/main/kotlin/com/deliveroo/cron/DayOfWeekParser.kt
@@ -1,7 +1,9 @@
 package com.deliveroo.cron
 
+import com.deliveroo.cron.ExtensionFunctions.replaceCommasWithSpaces
+
 object DayOfWeekParser {
     fun parse(dayOfWeekField: String): String {
-        return "day of week $dayOfWeekField"
+        return "day of week ${dayOfWeekField.replaceCommasWithSpaces()}"
     }
 }
--- src/test/kotlin/com/deliveroo/cron/DayOfWeekParserTest.kt
+++ src/test/kotlin/com/deliveroo/cron/DayOfWeekParserTest.kt
@@ -9,4 +9,9 @@ internal class DayOfWeekParserTest {
     fun `Day of week field should support a single day`() {
         assertThat(DayOfWeekParser.parse("1")).isEqualTo("day of week 1")
     }
+
+    @Test
+    fun `Day of week field should support multiple days`(){
+        assertThat(DayOfWeekParser.parse("1,2,3")).isEqualTo("day of week 1 2 3")
+    }
 }

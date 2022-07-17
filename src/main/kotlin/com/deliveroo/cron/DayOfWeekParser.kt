8219b5c2ff2b146161365b5802a0659f8e7f4b1c
Andrew Watson <andrewjw64@gmail.com>
2022-07-17 08:18:47 +0100

    Add range of days of the week functionality

--- src/main/kotlin/com/deliveroo/cron/DayOfWeekParser.kt
+++ src/main/kotlin/com/deliveroo/cron/DayOfWeekParser.kt
@@ -1,9 +1,21 @@
 package com.deliveroo.cron
 
 import com.deliveroo.cron.ExtensionFunctions.replaceCommasWithSpaces
+import com.deliveroo.cron.ExtensionFunctions.toSpaceSeparatedString
+
+private const val DAY_OF_WEEK = "day of week"
 
 object DayOfWeekParser {
+
+    private val rangeRegex = Regex("^([1-7])-([1-7])\$")
+
     fun parse(dayOfWeekField: String): String {
-        return "day of week ${dayOfWeekField.replaceCommasWithSpaces()}"
+        if (rangeRegex.matches(dayOfWeekField)) {
+            val matchResult = rangeRegex.find(dayOfWeekField)!!
+            val start = Integer.parseInt(matchResult.groupValues[1])
+            val end = Integer.parseInt(matchResult.groupValues[2])
+            return "$DAY_OF_WEEK ${(start..end).toSpaceSeparatedString()}"
+        }
+        return "$DAY_OF_WEEK ${dayOfWeekField.replaceCommasWithSpaces()}"
     }
 }
--- src/test/kotlin/com/deliveroo/cron/DayOfWeekParserTest.kt
+++ src/test/kotlin/com/deliveroo/cron/DayOfWeekParserTest.kt
@@ -14,4 +14,9 @@ internal class DayOfWeekParserTest {
     fun `Day of week field should support multiple days`(){
         assertThat(DayOfWeekParser.parse("1,2,3")).isEqualTo("day of week 1 2 3")
     }
+
+    @Test
+    fun `Day of week field should support a range of days greater than 1`(){
+        assertThat(DayOfWeekParser.parse("2-5")).isEqualTo("day of week 2 3 4 5")
+    }
 }

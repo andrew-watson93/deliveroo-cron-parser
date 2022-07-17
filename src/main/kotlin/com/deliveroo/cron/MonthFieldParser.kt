1df774b584487edd58441ee91d8b3625c4fe0397
Andrew Watson <andrewjw64@gmail.com>
2022-07-17 07:45:51 +0100

    Add month range functionality

--- src/main/kotlin/com/deliveroo/cron/MonthFieldParser.kt
+++ src/main/kotlin/com/deliveroo/cron/MonthFieldParser.kt
@@ -1,9 +1,20 @@
 package com.deliveroo.cron
 
 import com.deliveroo.cron.ExtensionFunctions.replaceCommasWithSpaces
+import com.deliveroo.cron.ExtensionFunctions.toSpaceSeparatedString
+
+private const val MONTH = "month"
 
 object MonthFieldParser {
+
+    private val rangeRegex = Regex("^([1-9]|1[0-2])-([1-9]|1[0-2])\$")
     fun parse(monthField: String): String {
-        return "month ${monthField.replaceCommasWithSpaces()}"
+        if (rangeRegex.matches(monthField)) {
+            val matchResult = rangeRegex.find(monthField)!!
+            val start = Integer.parseInt(matchResult.groupValues[1])
+            val end = Integer.parseInt(matchResult.groupValues[2])
+            return "$MONTH ${(start..end).toSpaceSeparatedString()}"
+        }
+        return "$MONTH ${monthField.replaceCommasWithSpaces()}"
     }
 }
--- src/test/kotlin/com/deliveroo/cron/MonthFieldParserTest.kt
+++ src/test/kotlin/com/deliveroo/cron/MonthFieldParserTest.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test
 
 class MonthFieldParserTest {
 
-
     @Test
     fun `Month field should support a single month`(){
         assertThat(MonthFieldParser.parse("5")).isEqualTo("month 5")
@@ -14,5 +13,10 @@ class MonthFieldParserTest {
     @Test
     fun `Month field should support multiple months`(){
         assertThat(MonthFieldParser.parse("5,6,7")).isEqualTo("month 5 6 7")
+    }
+
+    @Test
+    fun `Month field should support a range of digits greater than 1`(){
+        assertThat(MonthFieldParser.parse("1-5")).isEqualTo("month 1 2 3 4 5")
     }
 }

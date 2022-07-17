290e3a37ba5a1073baa3557b19842674ec820e99
Andrew Watson <andrewjw64@gmail.com>
2022-07-17 07:04:49 +0100

    Add functionality for day of month ranges

--- src/main/kotlin/com/deliveroo/cron/DayOfMonthFieldParser.kt
+++ src/main/kotlin/com/deliveroo/cron/DayOfMonthFieldParser.kt
@@ -1,9 +1,19 @@
 package com.deliveroo.cron
 
 import com.deliveroo.cron.ExtensionFunctions.replaceCommasWithSpaces
+import com.deliveroo.cron.ExtensionFunctions.toSpaceSeparatedString
 
 object DayOfMonthFieldParser {
+
+    private val rangeRegex = Regex("^([1-9]|[12]\\d|3[01])-([1-9]|[12]\\d|3[01])\$")
+
     fun parse(dayOfMonthField: String): String {
+        if (rangeRegex.matches(dayOfMonthField)) {
+            val matchResult = rangeRegex.find(dayOfMonthField)!!
+            val start = Integer.parseInt(matchResult.groupValues[1])
+            val end = Integer.parseInt(matchResult.groupValues[2])
+            return "day of month ${(start..end).toSpaceSeparatedString()}"
+        }
         return "day of month ${dayOfMonthField.replaceCommasWithSpaces()}"
     }
 }
--- src/test/kotlin/com/deliveroo/cron/DayOfMonthFieldParserTest.kt
+++ src/test/kotlin/com/deliveroo/cron/DayOfMonthFieldParserTest.kt
@@ -1,17 +1,21 @@
 package com.deliveroo.cron
 
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class DayOfMonthFieldParserTest {
     @Test
-    fun `Day of month field should support a single day of the month`(){
+    fun `Day of month field should support a single day of the month`() {
         assertThat(DayOfMonthFieldParser.parse("4")).isEqualTo("day of month 4")
     }
 
     @Test
-    fun `Day of month field should support multiple days of the month`(){
+    fun `Day of month field should support multiple days of the month`() {
         assertThat(DayOfMonthFieldParser.parse("4,5,6,7")).isEqualTo("day of month 4 5 6 7")
+    }
+
+    @Test
+    fun `Day of month field should support a range of digits greater than 1`() {
+        assertThat(DayOfMonthFieldParser.parse("1-5")).isEqualTo("day of month 1 2 3 4 5")
     }
 }
